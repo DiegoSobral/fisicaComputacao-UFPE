1077911a3b571e71eed5bd2990a4dbb3f410e9ca
Diego Ribeiro da Silva Sobral <114121416+DiegoSobral@users.noreply.github.com>
2024-04-05 15:29:50 -0300

    Create media.java

---
+++ UNIT_Aldo-LGP|POO/media.java
@@ -0,0 +1,10 @@
+public class Main
+{
+	public static void main(String[] args) {
+	    double n1 = -5, n2 = 7, media;
+	    
+	    
+	    
+	    media = (nota1 + nota2) / 2; 
+	    System.out.println("Sua média é:" + media);
+}}
